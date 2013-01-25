d3f0e197f3ddbb4d0d286fcdee34c3a43be770b6
Michiel de Jong <michiel@unhosted.org>
2013-01-25 21:39:32 +0700

    update after 2012 experiments:

--- old/config.js
+++
@@ -1,8 +0,0 @@
-exports.config = {
-  backends: { statics: 80 },
-  redirect: {
-    "opentabs-net.nodejitsu.com": "opentabs.net",
-    "www.opentabs.net": "opentabs.net"
-  },
-  domainsDir: 'statics/'
-};
